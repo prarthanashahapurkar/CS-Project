36763d8eb43d8976c0071c76a6add3277c609c79
prarthanashahapurkar <pshahapurkar28@gmail.com>
2021-12-02 12:51:49 +0530

    add start

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const app = express();
+const requestIp = require('request-ip');
 
 app.get("/", function(req,res) {
 
     console.log("WORKING")
     res.send("Great Success!!");
+    const ip = requestIp.getClientIp(req); 
+    console.log("ip: ",ip);
+    res.render("index.html");
 });
+
+
+// const RequestIp = require('@supercharge/request-ip');
+// const app= express()
+// app.use(express.urlencoded({ extended: false }));
+// app.set("view engine", "ejs");
+// app.use( express.static( "./views" ) );
+// app.get("/",function(req,res){
+//     console.log("double working");
+//     const ip = RequestIp.getClientIp(req);
+//     console.log("YOur IP: ",ip);
+//     res.render("index");
+// });
 
 app.listen(process.env.PORT || 5000);
 
@@ -29,9 +46,6 @@ app.listen(process.env.PORT || 5000);
 //     console.log("YOur IP: ",ip);
 //     res.render("index");
 // });
-
-// app.listen(process.env.PORT || 5000);
-
 
 // app.get("/",function(req,res){
 //     console.log("double working");
