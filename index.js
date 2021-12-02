dfb8413d9caf82e8e213916293dd62bc8d5e8bdd
prarthanashahapurkar <pshahapurkar28@gmail.com>
2021-12-02 13:02:29 +0530

    add start

--- index.js
+++ index.js
@@ -35,25 +35,3 @@ app.listen(process.env.PORT || 5000);
 //git push -u origin master
 
 //###########################
-
-// const express= require("express");
-// const RequestIp = require('@supercharge/request-ip');
-// const app= express()
-// app.use(express.urlencoded({ extended: false }));
-// app.set("view engine", "ejs");
-// app.use( express.static( "./views" ) );
-// app.get("/",function(req,res){
-//     console.log("double working");
-//     const ip = RequestIp.getClientIp(req);
-//     console.log("YOur IP: ",ip);
-//     res.render("index");
-// });
-
-// app.get("/",function(req,res){
-//     console.log("double working");
-//     const ip = RequestIp.getClientIp(req);
-//     console.log("YOur IP: ",ip);
-//     res.render("index");
-// });
-
-// app.listen(process.env.PORT || 5000);
