6b57029b57a553fae80bb5a02ded7e0cf8e7b35d
prarthanashahapurkar <pshahapurkar28@gmail.com>
2021-12-02 13:14:34 +0530

    add start

--- index.js
+++ index.js
@@ -5,28 +5,19 @@ app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 app.use( express.static( "./views" ) );
 
+console.log("hellooo");
+
 app.get("/", function(req,res) {
     console.log("WORKING")
-    res.send("Great Success!!");
+    //res.send("Great Success!!");
     const ip = requestIp.getClientIp(req); 
     console.log("ip: ",ip);
     res.render("index");
 });
 
-
-// const RequestIp = require('@supercharge/request-ip');
-// const app= express()
-// app.use(express.urlencoded({ extended: false }));
-// app.set("view engine", "ejs");
-// app.use( express.static( "./views" ) );
-// app.get("/",function(req,res){
-//     console.log("double working");
-//     const ip = RequestIp.getClientIp(req);
-//     console.log("YOur IP: ",ip);
-//     res.render("index");
-// });
-
 app.listen(process.env.PORT || 5000);
+
+
 
 //###########################
 
